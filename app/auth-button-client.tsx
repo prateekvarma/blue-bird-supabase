7972b4a5e090101392d6d071615538b11a54b89f
prateekvarma <prtk.varma@gmail.com>
2023-07-26 14:27:24 -0600

    Used router to refresh page after logout

--- app/auth-button-client.tsx
+++ app/auth-button-client.tsx
@@ -2,9 +2,15 @@
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import type { Session } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
 
-export default function AuthButtonClient({ session }: { session: Session | null }) {
+export default function AuthButtonClient({
+  session,
+}: {
+  session: Session | null;
+}) {
   const supabase = createClientComponentClient();
+  const router = useRouter();
 
   const handleSignin = async () => {
     await supabase.auth.signInWithOAuth({
@@ -17,6 +23,7 @@ export default function AuthButtonClient({ session }: { session: Session | null 
 
   const handleSignout = async () => {
     await supabase.auth.signOut();
+    router.refresh();
   };
 
   return session ? (
